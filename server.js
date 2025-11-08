98235af74c9009b77be3473c533c32f85d1932ad
Prathamesh Kapadne <prathamesh.kapadne@mitaoe.ac.in>
2025-11-09 01:47:25 +0530

    chnages in code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 
-app.get('/', (req, res) => res.send('Hello From Serve 3001'))
+app.get('/', (req, res) => res.send('Hello From Serve 3000'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
