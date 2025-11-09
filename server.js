d8aaa599effd8a74f1c277f9b03b16b80bb3f90b
Prathamesh Kapadne <prathamesh.kapadne@mitaoe.ac.in>
2025-11-09 17:23:15 +0530

    chnages in jenkins and server file

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello From Serve 3001 we will rock you! Heyy didd you know running on port 3001'))
+app.get('/', (req, res) => res.send('Hello From Serve 3001'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
